182113f3b7738fda8770a53619d859b5365d7f7e
Toxblh <toxblh@gmail.com>
2019-07-16 13:56:17 +0100

    Ready for docker

--- lib.js
+++ lib.js
@@ -1,7 +1,7 @@
 const puppeteer = require('puppeteer')
 
 async function getCreditClubScore({ login, pass, word }) {
-  const browser = await puppeteer.launch()
+  const browser = await puppeteer.launch({args: ['--no-sandbox']})
   const page = await browser.newPage()
 
   await page.goto('https://clubs.moneysavingexpert.com/creditclub/login', {
